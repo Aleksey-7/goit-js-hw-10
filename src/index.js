3229e725da781d4cb987ec6ca8c61cc1d697fd7e
Aleksey-7 <84351678+Aleksey-7@users.noreply.github.com>
2022-12-04 19:57:54 +0200

    fix styles, update package.json
    fix styles, update package.json

--- src/index.js
+++ src/index.js
@@ -1,3 +1,57 @@
+import { refs } from './js/refs';
+import { fetchCountries } from './js/fetchCountries';
+import { countryСardMarkup, countryListMarkup } from './js/markup';
+
 import './css/styles.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import debounce from 'lodash.debounce';
 
 const DEBOUNCE_DELAY = 300;
+
+refs.searchBox.addEventListener(
+  'input',
+  debounce(onInputChange, DEBOUNCE_DELAY)
+);
+
+function onInputChange() {
+  const countryName = refs.searchBox.value;
+  if (countryName === '') {
+    refs.countryInfo.innerHTML = '';
+    refs.countryList.innerHTML = '';
+    return;
+  }
+
+  fetchCountries(countryName)
+    .then(countries => {
+      if (countries.length > 10) {
+        Notify.info(
+          'Too many matches found. Please enter a more specific name.'
+        );
+        refs.countryInfo.innerHTML = '';
+        refs.countryList.innerHTML = '';
+        return;
+      }
+
+      if (countries.length <= 10) {
+        const countryList = countries.map(country =>
+          countryListMarkup(country)
+        );
+        refs.countryList.innerHTML = countryList.join('');
+        refs.countryInfo.innerHTML = '';
+      }
+
+      if (countries.length === 1) {
+        const countryСard = countries.map(country =>
+          countryСardMarkup(country)
+        );
+        refs.countryInfo.innerHTML = countryСard.join('');
+        refs.countryList.innerHTML = '';
+      }
+    })
+    .catch(error => {
+      Notify.failure('Oops, there is no country with that name');
+      refs.countryInfo.innerHTML = '';
+      refs.countryList.innerHTML = '';
+      return error;
+    });
+}
---
+++ src/js/markup.js
@@ -0,0 +1,30 @@
+export function countryСardMarkup({
+  flags,
+  name,
+  capital,
+  population,
+  languages,
+}) {
+  return `
+    <div class="country-info__wrapper">
+      <img class="country-info__image" src="${flags.svg}" alt="${
+    name.common
+  }" width="30" />
+      <h2 class="country-info__name">${name.common}</h2>
+    </div>
+    <p class="country-info__capital"><b>Capital:</b> ${capital}</p>
+    <p class="country-info__population"><b>Population:</b> ${population}</p>
+    <p class="country-info__languages"><b>Languages:</b> ${Object.values(
+      languages
+    )}</p>
+  `;
+}
+
+export function countryListMarkup({ flags, name }) {
+  return `
+    <li class="country-list__item">
+      <img class="country-list__image" src="${flags.svg}" alt="${name.common}" width="30" />
+      <h2 class="country-list__name">${name.common}</h2>
+    </li>
+  `;
+}
